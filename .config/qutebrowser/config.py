2f899ac8cfaa95bb7a31597fc1e963d7b7905fae
Wilson Neto <wilsonpsn@hotmail.com>
2025-10-13 21:21:27 -0300

    feat(hypr): add hypridle and hyprlock configs
    Added new configuration files for hypridle and hyprlock to enable
    automatic screen locking, backlight control, and suspend behavior.
    Updated hyprland.conf to launch hypridle on startup. Also improved
    qutebrowser config with Google as default search and start page.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,7 @@
 config.load_autoconfig()
 
 c.editor.command = ["kitty", "-e", "nvim", "{}"]
+
+c.url.searchengines = {"DEFAULT": "https://www.google.com/search?hl=en&q={}"}
+
+c.url.start_pages = "https://www.google.com"
